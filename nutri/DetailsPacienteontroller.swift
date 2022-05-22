36d717561db623225440c91ff3b98627985b8375
AlejandraCeniceros1 <97917312+AlejandraCeniceros1@users.noreply.github.com>
2022-05-22 09:09:54 -0700

    updatess

--- nutri/DetailsPacienteontroller.swift
+++ nutri/DetailsPacienteontroller.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class DetailsVariantController : UIViewController {
+class DetailsPacienteontroller : UIViewController {
     var paciente : Paciente?
     
     @IBOutlet weak var imgEvidence: UIImageView!
